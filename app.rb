158447619ebbaaeedcb46f82fcbc543a530f0908
Pauline <pbaker@hotmail.co.uk>
2018-12-16 18:01:51 +0000

    removed sessions and used  global variables

--- app.rb
+++ app.rb
@@ -1,20 +1,18 @@
 require 'sinatra'
 
-enable :sessions
-
 get '/' do
   erb(:form)
 end
 
 post '/days_til_birthday' do
   p params
-  p session["name"] = params["name"]
-  p session["birthday"] = params["birthday"]
-  p @name = session["name"]
-  p @birthday = session["birthday"]
+  $name = params["name"]
+  $birthday = params["birthday"]
   erb(:days_til_birthday)
 end
 
 get '/birthday-greeting' do
+  $name = params["name"]
+  $birthday = params["birthday"]
   erb(:birthday_greeting)
 end
